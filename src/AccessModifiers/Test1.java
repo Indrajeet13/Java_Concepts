0af327007d9f9b4879e7e2711814682375ca7759
Indrajeet13 <Indrajeetyadav677.iy@gmail.com>
2024-11-16 09:55:57 +0530

    Uploading updated file

--- src/AccessModifiers/Test1.java
+++ src/AccessModifiers/Test1.java
@@ -1,10 +1,45 @@
 package AccessModifiers;
 
-public class Test1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Test1 
+{
+	
+	//Private ---------------------------------------
+	
+//	private int x = 100;	//Access only within  a class - private 
+//	
+//	private void m1()	//Access only within  a class - private 
+//	{
+//		System.out.println("This is m1 method");
+//	}
+	
+	
+	//Default ---------------------------------------
+	
+//	int x = 100;	//Access within a package - default
+//	
+//	void m1()	//Access within a package - default
+//	{
+//		System.out.println("This is m1 method");
+//	}
+	
+	//Protected -----------------------------------------
+	
+//	protected int x = 100;	//Outside of the package using inheritance
+//	
+//	protected void m1()	//Access within a package directly, and access outside of the package using inheritance
+//	{
+//		System.out.println("This is m1 method");
+//	}
+	
+	
+	//Public ----------------------------------------------
+	
+	public int x = 100;	// Access anywhere in the project
+	
+	public void m1()	//Access anywhere in the project
+	{
+		System.out.println("This is m1 method");
 	}
-
+	
+	
 }
